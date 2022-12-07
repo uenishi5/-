814dc307ea89cf9498da77e8e4cdb8989b9befff
sawadayuunari <yuuyazetian999@gmail.com>
2022-12-07 14:04:00 +0900

    r4プロジェクトビルド作成

--- src/main/java/jp/hcs/ac/s3a321/r4_project/web/R4Controller.java
+++ src/main/java/jp/hcs/ac/s3a321/r4_project/web/R4Controller.java
@@ -5,24 +5,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
-@RequestMapping("/r4project")
 @RequiredArgsConstructor
 public class R4Controller {
     private SearchService service;
 
-    @GetMapping("/r4Index")
+    @GetMapping("/r4project/r4Index")
     public String showList(Model model) {
-        return "r4project/r4Index";
-    }
-
-    @PostMapping()
-    public String SearchResult(Model model,String detail){
-        model.addAttribute("list",service.getList(detail));
-        return "r4project/r4Index";
+        return "/r4project/r4Index";
     }
 }
