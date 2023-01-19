1b506b466aa3d3b921dc0871701063fe38ad40f5
mike130302 <84884879+mike130302@users.noreply.github.com>
2023-01-19 13:52:38 +0900

    Delete src/main/java/jp directory

--- src/main/java/jp/hcs/ac/s3a321/main/MessageVO.java
+++
@@ -1,6 +0,0 @@
-package jp.hcs.ac.s3a321.main;
-
-public class MessageVO {
-	public static final String ATTRIBUTE_MESSAGE = "message";
-	public static final String ATTRIBUTE_ERROR_MESSAGE = "errorMessage";
-}
--- src/main/java/jp/hcs/ac/s3a321/zipcode/ZipCodeVO.java
+++
@@ -1,9 +0,0 @@
-package jp.hcs.ac.s3a321.zipcode;
-
-import jp.hcs.ac.s3a321.main.PortalVO;
-
-public class ZipCodeVO {
-	public static final String REDIRECT_INDEX = PortalVO.REDIRECT_INDEX;
-	public static final String MAPPING_RESULT = "/zip";
-	public static final String RESOURCE_RESULT = "zipcode/result";
-}
