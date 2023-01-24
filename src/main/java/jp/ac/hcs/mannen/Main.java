f8cbb84843b3a19c4bf49233a0d3ac4de268a54a
s20203029 <s20203029@HCS19087.stdlan.hcs.ac.jp>
2023-01-24 14:21:01 +0900

    クラスの変更

--- src/main/java/jp/ac/hcs/mannen/Main.java
+++ src/main/java/jp/ac/hcs/mannen/Main.java
@@ -1,3 +1,4 @@
+package jp.ac.hcs.mannen;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
