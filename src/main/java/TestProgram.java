3514dc4ea35f92098d5ce83a92206f4b1d5cd489
s20203029 <s20203029@HCS19087.stdlan.hcs.ac.jp>
2023-01-27 13:57:20 +0900

    修正

--- src/main/java/TestProgram.java
+++ src/main/java/TestProgram.java
@@ -6,6 +6,12 @@ import java.util.List;
 
 import org.springframework.boot.system.SystemProperties;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.Data;
+
 public class TestProgram {
 
 	public static void download(String url, boolean toMp3) {
@@ -60,7 +66,68 @@ public class TestProgram {
 
 	}
 
+	public static TestA jsonToTestAClass() {
+		// 文字列 から クラス に変換するプログラム
+
+		// フロントからの渡されるjson形式のデータ
+		String json = "{\"label\":\"testLabel\", \"id\":1}";
+
+		// インスタンス生成
+		final ObjectMapper objectMapper = new ObjectMapper();
+
+		try {
+			// "json形式の文字列" から "TestClassA クラス" に変換
+			// 変換することによってデータをオブジェクトとして扱うことができる
+			// 必ず JsonMappingexception と JsonProcessingException をキャッチする
+			return objectMapper.readValue(json, TestA.class);
+		}
+		catch (JsonMappingException e) {
+			// データのエンコード/デコードに問題があった際に発生する（直訳）
+			e.printStackTrace();
+		}
+		catch (JsonProcessingException e) {
+			// JSON コンテンツを処理（パース、生成）する際に発生する（直訳）
+			e.printStackTrace();
+		}
+
+		return new TestA();
+	}
+
+	public static String testAClassToJson() {
+		// クラス から 文字列 に変換するプログラム
+
+		// フロントからの渡されるjson形式のデータ
+		TestA testA = new TestA();
+		testA.setId(2);
+		testA.setLabel("testLabelTest");
+
+		// インスタンス生成
+		final ObjectMapper objectMapper = new ObjectMapper();
+
+		try {
+			// "TestClassA クラス" から "json形式の文字列"に変換
+			// 必ず JsonProcessingException をキャッチする
+			return objectMapper.writeValueAsString(testA);
+		}
+		catch (JsonProcessingException e) {
+			// JSON コンテンツを処理（パース、生成）する際に発生する（直訳）
+			e.printStackTrace();
+		}
+
+		return "";
+	}
+
+	@Data
+	public static class TestA {
+		private String label;
+		private int id;
+	}
+
 	public static void main(String[] args) {
-		download("", false);
+
+		System.out.println(jsonToTestAClass());
+		System.out.println(testAClassToJson());
+
+		// download("", false);
 	}
 }
