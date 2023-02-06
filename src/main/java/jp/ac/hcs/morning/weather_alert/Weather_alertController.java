336aa22952c62a15fcccd7216c5fe8c26012704d
s20203029 <s20203029@HCS19087.stdlan.hcs.ac.jp>
2023-02-06 13:57:53 +0900

    リファクタリング

--- src/main/java/jp/ac/hcs/morning/weather_alert/Weather_alertController.java
+++ src/main/java/jp/ac/hcs/morning/weather_alert/Weather_alertController.java
@@ -1,34 +1,37 @@
 package jp.ac.hcs.morning.weather_alert;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import jp.ac.hcs.config.Mapping;
 import jp.ac.hcs.morning.main.MainController;
-/**警報注意報のコントローラー*/
+
+/** 警報注意報のコントローラー */
 @Controller
 public class Weather_alertController {
+
+	@Autowired
+	private Weather_alertService weather_alertService;
+
 	@RequestMapping("/Weather_alert")
-	public String getMainData(Model model){
-		Weather_alertEntity entity = new Weather_alertEntity();
-		Weather_alertService service = new Weather_alertService();
-		entity = service.getWeather_alertData();
-		model.addAttribute("Weather_alert",entity);
-		Weather_alertData a = entity.getWeather_alertnameList().get(0);
-		String name = a.getName();
-		if(!(entity.isError())) {
-		if(name.equals("発表なし")) {
-			model.addAttribute("flg", true);
-		}else {
-			model.addAttribute("flg", false);
+	public String getMainData(Model model) {
+		final Weather_alertEntity entity = this.weather_alertService.getWeather_alertData();
+		final Weather_alertData data = entity.getWeather_alertnameList().get(0);
+		final String name = data.getName();
+
+		if (!(entity.isError())) {
+			model.addAttribute("flg", name.equals("発表なし"));
 		}
-		}else {
-			MainController main = new MainController();
-			model.addAttribute("errormessage","エラーが発生しました。");
+		else {
+			final MainController main = new MainController();
+			model.addAttribute("errormessage", "エラーが発生しました。");
 			main.getMainData(model);
 		}
+
+		model.addAttribute("Weather_alert", entity);
 		return Mapping.RESOURCE_WEATHER_ALERT;
 	}
-		
+
 }
