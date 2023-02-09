7d21dc80bddaae09070d8fbd4bd72ea2d6697b36
s20203029 <s20203029@HCS19087.stdlan.hcs.ac.jp>
2023-02-09 16:59:41 +0900

    絞り込み追加

--- src/main/java/jp/ac/hcs/smoker/controller/NewsForm.java
+++ src/main/java/jp/ac/hcs/smoker/controller/NewsForm.java
@@ -1,6 +1,7 @@
 package jp.ac.hcs.smoker.controller;
 
 import com.ren130302.webapi.newsapi.enums.NewsLanguage;
+import com.ren130302.webapi.newsapi.enums.NewsSearchIn;
 import com.ren130302.webapi.newsapi.enums.NewsSort;
 import com.ren130302.webapi.newsapi.request.Everything;
 
@@ -10,9 +11,10 @@ import lombok.Data;
 public class NewsForm {
 	private String q;
 	private NewsSort sort = NewsSort.PUBLISHED_AT;
+	private NewsLanguage language = NewsLanguage.EN;
+	private NewsSearchIn[] searchIns = NewsSearchIn.values();
 
 	public Everything.Builder query(Everything.Builder b) {
-		b.sortBy(this.getSort()).languages(NewsLanguage.EN).q(this.getQ());
-		return b;
+		return b.sortBy(this.getSort()).languages(this.getLanguage()).q(this.getQ()).searchIn(this.getSearchIns());
 	}
 }
