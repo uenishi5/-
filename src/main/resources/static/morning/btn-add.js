6c22b44c06ea3e51080fe552fd6e8df05ba3dba0
s20212066 <s20212066@HCS19094.stdlan.hcs.ac.jp>
2023-02-15 15:15:45 +0900

    button

--- src/main/resources/static/morning/btn-add.js
+++ src/main/resources/static/morning/btn-add.js
@@ -1,14 +1,16 @@
 window.onload = $(function(){
 	    var cnt = $(".hoge").length;
-	    var i = 0;
+	    var i = 0;	
 	    
 	    const text = document.getElementById('text');
 	    
 	    while(i < cnt){
 		var cs = document.getElementsByClassName("hoge");
-			myid = cs[i].getAttribute("id");
+			myid = 'buttonID' + i;
+			divid = 'testID' + i;
 			myname = cs[i].getAttribute("name");
-			text.innerHTML += '<p>' + myname + 'ﾎﾞﾀﾝ</p><group class=\"remove-button\"><div><input style=\"float: left;\" value=\"表示\" type=\"button\" name=\"member\" onclick=\"document.getElementById(\'' + myid + '\').style.display = \'inline\';\" checked ></div><div><input style=\"float: right;\" value=\"非表示\" type=\"button\" name=\"member\" onclick=\"document.getElementById(\'' + myid + '\').style.display = \'none\';\"></group>';
+			text.innerHTML += '<p>' + myname + 'ﾎﾞﾀﾝ</p><input id=\"' + myid + '\" type=\"button\" value = "表示" onclick=\"clickBtn()\">';
 			i++;
 		}
 	});
+
