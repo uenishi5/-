36459b2f7a8d263e3ebe7c7256f9e25b23c5bb5a
s20212066 <s20212066@HCS19094.stdlan.hcs.ac.jp>
2023-02-16 10:44:04 +0900

    changebox

--- src/main/resources/static/morning/btn-add.js
+++ src/main/resources/static/morning/btn-add.js
@@ -5,12 +5,11 @@ window.onload = $(function(){
 	    const text = document.getElementById('text');
 	    
 	    while(i < cnt){
-		var cs = document.getElementsByClassName("hoge");
+		var cs = document.getElementsByClassName("morning");
 			myid = 'buttonID' + i;
 			divid = 'testID' + i;
 			myname = cs[i].getAttribute("name");
-			text.innerHTML += '<p>' + myname + 'ﾎﾞﾀﾝ</p><input id=\"' + myid + '\" type=\"button\" value = "表示" onclick=\"clickBtn()\">';
+			text.innerHTML += '<p>' + myname + 'ﾎﾞﾀﾝ</p><input id=\"' + myid + '\" type=\"button\" value = "表示" name=\"' + i + '\" onclick=\"clickBtn(' + myid + ')\">';
 			i++;
 		}
 	});
-
